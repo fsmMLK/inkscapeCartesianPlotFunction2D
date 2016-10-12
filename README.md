253a931b19e61b5f755139ee53d7def30374d3e2
fsmMLK <fsmMLK@users.noreply.github.com>
2016-10-12 18:29:56 -0300

    initial version

---
+++ README.md
@@ -0,0 +1,145 @@
+# cartesianPlotFunction2D
+ Inkscape extension to plot functions of one independent variable
+
+<img src="docs/images/Examples.png" width="900px"/>
+
+This extension was inspired by Tavmjong Bah's (and collaborators) extension **Function Plotter** already presented in Inkscape. Function Plotter extension is not required here.
+
+### main features
+
+The main features are
+
+ - linear and log10 scales
+ - optional grid lines in X and Y directions
+ - adjustable tick mark intervals and sizes
+ - LaTeX support
+
+# Installation and requirements
+
+This extension was partially developed in Inkscape 0.48 and partially in 0.91 in Linux (Kubuntu 12.04 and 14.04). It should work on both versions of Inkscape. Also, they should work in different OSs too as long as all requirements are installed.
+
+This extension requires another extension to run, inkscapeMadeEasy <https://github.com/fsmMLK/inkscapeMadeEasy>, which contains several backstage methods and classes.
+
+In order to use cartesianPlotFunction2D extension, you must also download inkscapeMadeEasy files and put them inside Inkscape's extension directory. Please refer to inkscapeMadeEasy installation instructions. In the end you must have the following files and directories in your Inkscape extension directory.
+
+```
+inkscape/extensions/
+            |-- inkscapeMadeEasy_Base.py
+            |-- inkscapeMadeEasy_Draw.py
+            |-- inkscapeMadeEasy_Plot.py
+            |-- textextLib
+            |   |-- __init__.py
+            |   |-- basicLatexPackages.tex
+            |   |-- textext.inx
+            |   |-- textext.py
+            |
+            |-- cartesianPlotFunction2D.py
+            `-- cartesianPlotFunction2D.inx
+```
+
+# Usage
+
+This extension is presented in two tabs, **Function Definition** and  **Axes configuration**. The first is used to define the function and input/output ranges. The second is used to configure the axes.
+
+<img src="docs/images/Config_Function.png" width="600px"/>
+
+<img src="docs/images/Config_Axes.png" width="600px"/>
+
+### The Function Definition tab
+
+Here you can define your function and set both the input and output ranges. 
+
+**Function:** Function definition. This must be a python compatible expression. Your independent variable **MUST** be `x`.
+
+> Tip: This extension imports all method from 'math' module from python. Therefore you don have to specify the module name math.<some_method>.
+
+> Example: to generate a sine function, just type `sin(x)`. You don't have to type `math.sin(x)`
+
+**Number of points:** Number of points where the function will be evaluated within the limits (see below)
+
+  - If logarithmic scale is not checked for X axis, then the points will be uniformly distributed within the limits
+  - If logarithmic scale is     checked for X axis, then the points will be logarithmically distributed within the limits
+
+
+**Use ellipsis marks at the ends:** Use ellipsis marks at the end of the plots to indicate continuation
+
+<img src="docs/images/ellipsis.png" width="600px"/>
+
+**Draw Cartesian plane:** Toggle to generate the axes grid. Usually you want this to be checked. =)
+
+<img src="docs/images/drawAxes.png" width="600px"/>
+
+**General aspect factor:** General aspect ratio between line widths and text width. I designed this extension to have an overall aspect ratio that looked nice to my eyes. It is a function of X and Y tick lengths. With this control you can scale both line widths and text height to fit your needs.
+
+<img src="docs/images/generalAspectRatio.png" width="700px"/>
+
+**X min and max:** Set the limits of the X axis. The extension will inform if these limits are invalid.
+  
+  - The upper limits must be greater than the lower limits
+  - If logarithmic scale is checked, then the lower limit will be rounded down to the nearest power of 10 and the upper rounded up to the nearest power of 10 in order to complete the decades. Ex: 0.2 to 12, then the limits will be rounded to 0.1 to 100. **Attention:** This will not change the range of values in which the function will be evaluated. The limits will be rounded only to produce the Cartesian axes with complete decades.
+  - If logarithmic scale is checked:
+    * if xMin<=0.0 then the lower limit xMin will be set to 0.001.
+    * if xMax<=0.0 then the upper limit xMax will be set to 100 times xMin.
+
+<img src="docs/images/xMin_xMax.png" width="600px"/>
+
+**Y min and max:** (linear scale only) Set the limits of the Y axis. The extension will inform if these limits are invalid. These values are used to limit the solution, that is, if `f(x)` does not lie within Ymin and Ymax, then the solution will be clipped at these values.
+
+<img src="docs/images/yMin_yMax.png" width="600px"/>
+
+
+**multiply X by pi:** Multiply the limits by pi. Ex: `xMin=-2` ans `xMax=2`, with this checkbox selected results in x limits of `xMin=-2pi` ans `xMax=2pi`. This is specially useful for trigonometric functions. If this checkbox is selected, *X tick step* (see below) will be also multiplied  by pi.
+
+<img src="docs/images/multiply_X_ByPi.png" width="600px"/>
+
+### The Axes Configuration tab
+
+
+**X/Y axis label:** label of the axes. This string must be LaTeX compatible. Any LaTeX commands or environments are valid. If you want to write in mathematical environment, enclose your text with $...$. You don't have to escape any backslashes.
+
+> Tip: Since `siunitx` package is included in basicLatexPackages.tex file by default in inkscapeMadeEasy, you can use any unit command available there 
+
+Ex: `Foobar $\sqrt{x^2}$ so fancy! (\si{\newton\per\squaremetre})`
+
+<img src="docs/images/Legend_01.png" width="400px"/>
+
+**Logarithmic scale:** Set the axis to be represented in log10 scale.
+
+**Add grid to X/Y axis:** Draw grid lines in X or Y axes.
+   - *linear scale:* The grid lines will be placed at each tick mark
+   - *logarithmic scale:* The grid lines will be placed dividing each decade in 10 parts
+
+**Add ticks to X/Y axis:** Draw tick marks with associated values in X or Y axes.
+
+**X/Y tick step:** Tick marks interval  in units of your chart. This value is not referenced to  in logarithmic scale
+
+Ex: limits from -1 to 1, with tick step of 0.5 will produce ticks at -1, -0.5, 0, 0.5, 1
+
+> Note: The ticks will radiate from the origin x=0 or y=0 unless the origin does not lie within the limits. In such cases, the ticks will radiate starting from the  limit closest to the origin.
+>
+> Examples in x direction. The same rules apply to Y direction
+>
+> <img src="docs/images/TickStep.png" width="600px"/>
+
+
+**X/Y tick length** The distance between the tick marks, in px.
+   - *linear scale:* The distance between ticks in px.
+   - *logarithmic scale:* The size of each decade in px.
+
+<img src="docs/images/TickLength.png" width="350px"/>
+
+**X/Y tick suffix value:** Optional extra suffix to be added to the tick values. You can use any LaTeX text/commands valid in mathematical environment $...$. You don't have to enclose your text between $...$. You don't have to escape any backslashes.
+
+<img src="docs/images/TickSuffix.png" width="800px"/>
+
+# Observations
+
+ - The axes will be placed crossing the origin (0,0) or crossing the coordinate (x,y) closest to the origin if the origin does not lies within the limits.
+ - The axes will be created at the center of your screen.
+
+# Examples
+
+<img src="docs/images/Examples.png" width="900px"/>
+
+
+
---
+++ cartesianPlotFunction2D.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python
+
+# --------------------------------------------------------------------------------------
+#
+#    cartesianPlotFunction2D: - Inkscape extension to plot functions of one independent variable
+#
+#    Copyright (C) 2016 by Fernando Moura
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# --------------------------------------------------------------------------------------
+
+import inkex
+import inkscapeMadeEasy_Base as inkBase
+import inkscapeMadeEasy_Draw as inkDraw
+import inkscapeMadeEasy_Plot as inkPlot
+from math import *
+import numpy
+import random
+import sys
+# heaviside function
+def u(x):
+  if x<0:
+    return 0.0
+  else:
+    return 1.0
+
+#rectangular Pulse
+# 
+def rectPulse(x,amplitude=1.0,lenght=1.0,offset=0.0,delay=0.0):
+  return amplitude*(u(x-delay)-u(x-lenght-delay))+offset
+
+#square wave, with amplitude -A/2 to A/2, and give period
+def squareWave(x,amplitude=1.0,offset=0,period=1.0,delay=0.0):
+  return rectPulse((x% period),amplitude,period/2.0,offset=-amplitude/2.0+offset,delay=delay)
+
+def rand():
+  return random.random();
+#---------------------------------------------
+class PlotFunction(inkBase.inkscapeMadeEasy):
+  def __init__(self):
+    inkex.Effect.__init__(self)
+
+    self.OptionParser.add_option("--tab",action="store", type="string",dest="tab", default="object") 
+   
+    self.OptionParser.add_option("--function", action="store", type="string", dest="function", default='pow(x,2)')
+    self.OptionParser.add_option("--nPoints", action="store", type="int", dest="nPoints", default=20)
+    
+    self.OptionParser.add_option("--xMin", action="store", type="float", dest="xMin", default='0.0')
+    self.OptionParser.add_option("--xMax", action="store", type="float", dest="xMax", default='0.0')
+    self.OptionParser.add_option("--xPi", action="store", type="inkbool", dest="xPi", default=False)
+    self.OptionParser.add_option("--yMin", action="store", type="float", dest="yMin", default='0.0')
+    self.OptionParser.add_option("--yMax", action="store", type="float", dest="yMax", default='0.0')
+    self.OptionParser.add_option("--useElipsis", action="store", type="inkbool", dest="useEllipsis", default=False)
+    self.OptionParser.add_option("--drawAxis", action="store", type="inkbool", dest="drawAxis", default=True)
+    
+    
+    self.OptionParser.add_option("--xLabel", action="store", type="string", dest="xLabel", default='')
+    self.OptionParser.add_option("--xScale", action="store", type="float", dest="xScale", default='5')
+    self.OptionParser.add_option("--xLog10scale", action="store", type="inkbool", dest="xLog10scale", default=False)
+    self.OptionParser.add_option("--xTicks", action="store", type="inkbool", dest="xTicks", default=False)
+    self.OptionParser.add_option("--xTickStep", action="store", type="float", dest="xTickStep", default='1')
+    self.OptionParser.add_option("--xGrid", action="store", type="inkbool", dest="xGrid", default=True)
+    self.OptionParser.add_option("--xExtraText", action="store", type="string", dest="xExtraText", default='')
+
+    self.OptionParser.add_option("--yLabel", action="store", type="string", dest="yLabel", default='')
+    self.OptionParser.add_option("--yScale", action="store", type="float", dest="yScale", default='5')
+    self.OptionParser.add_option("--yLog10scale", action="store", type="inkbool", dest="yLog10scale", default=False)
+    self.OptionParser.add_option("--yTicks", action="store", type="inkbool", dest="yTicks", default=False)
+    self.OptionParser.add_option("--yTickStep", action="store", type="float", dest="yTickStep", default='1')
+    self.OptionParser.add_option("--yGrid", action="store", type="inkbool", dest="yGrid", default=True)
+    self.OptionParser.add_option("--yExtraText", action="store", type="string", dest="yExtraText", default='')
+    
+    self.OptionParser.add_option("--generalAspectFactor", action="store", type="float", dest="generalAspectFactor", default=1.0)
+
+  def effect(self):
+    
+    so = self.options
+
+    # sets the position to the viewport center, round to next 10.
+    position=[self.view_center[0],self.view_center[1]]
+    position[0]=int(ceil(position[0] / 10.0)) * 10
+    position[1]=int(ceil(position[1] / 10.0)) * 10
+    
+    #root_layer = self.current_layer
+    root_layer = self.document.getroot()
+    
+    myLambda=eval('lambda x: ' + so.function)
+    
+    #generate x data
+    if not so.xLog10scale:
+      xData=numpy.linspace(so.xMin,so.xMax,so.nPoints)   
+    else:
+      xMin=so.xMin
+      xMax=so.xMax
+      #check limits
+      if so.xMin<=0:
+        self.displayMsg('Error: xMin=%d is invalid in logarithmic scale\nUsing 0.001 instead' %(so.xMin))
+        xMin=0.001
+      #check limits
+      if so.xMax<=0:
+        self.displayMsg('Error: xMax=%d is invalid in logarithmic scale\nUsing  100 times xMin instead' %(so.xMax))
+        xMax=100*xMin
+
+      xData=numpy.logspace(log10(xMin),log10(xMax),so.nPoints)   
+      
+    if so.xPi:
+      xData=[x*pi for x in xData]
+      
+    #generate y data
+    #yData= map(myLambda, xData)
+    
+    yData=[]
+    for x in xData:
+      if myLambda(x) == float('Inf'):
+        yData.append(0.0)
+        inkDraw.text.write(self,'Inf result detected: The function at x=%d is infinite.' %x,position,root_layer,fontSize=5)
+        inkDraw.text.write(self,'Setting the point to 0... PLEASE CHECK YOUR PLOT!' %x,[position[0],position[1]+8],root_layer,fontSize=5)
+      else:
+        yData.append(myLambda(x))
+  
+      
+    # check if limits are valid
+    if so.yMin >= so.yMax:
+        self.displayMsg('Error: yMin and yMax are invalid.')
+        return 0
+          
+    # block limits
+    yData=[min(y,so.yMax) for y in yData]
+    yData=[max(y,so.yMin) for y in yData]
+    
+    if so.yLog10scale:
+      ylim=None
+    else:
+      ylim=[so.yMin,so.yMax]
+    
+    # x labels if multiply by pi
+    if so.xPi:
+      xData=numpy.linspace(so.xMin,so.xMax,so.nPoints)
+      xExtraText="\pi "+so.xExtraText
+    else:
+      xExtraText=so.xExtraText
+        
+    # line style
+    lineWidthPlot=so.generalAspectFactor*min(so.xScale,so.yScale)/30.0 
+    lineColor=inkDraw.color.defined('blue')
+    if so.useEllipsis:
+      StartLineInf,EndLineInf = inkDraw.marker.createInfLineMarker(self,'InfiniteLine',RenameMode=1,fillColor=lineColor)
+      lineStylePlot = inkDraw.lineStyle.set(lineWidth=lineWidthPlot,lineColor=lineColor,markerStart=StartLineInf,markerEnd=EndLineInf)
+    else:
+      lineStylePlot = inkDraw.lineStyle.set(lineWidth=lineWidthPlot,lineColor=lineColor)
+
+    inkPlot.plot.cartesian(self,root_layer,xData,yData,position,
+                           xLabel=so.xLabel,yLabel=so.yLabel,xlog10scale=so.xLog10scale,ylog10scale=so.yLog10scale,
+                           xTicks=so.xTicks,yTicks=so.yTicks,xTickStep=so.xTickStep,yTickStep=so.yTickStep,
+                           xScale=so.xScale,yScale=so.yScale,xExtraText=xExtraText,yExtraText=so.yExtraText,
+                           xGrid=so.xGrid,yGrid=so.yGrid,generalAspectFactorAxis=so.generalAspectFactor,lineStylePlot=lineStylePlot,
+                           forceYlim=ylim,drawAxis=so.drawAxis)
+                            
+if __name__ == '__main__':
+  plot = PlotFunction()
+  plot.affect()
+    
+    
